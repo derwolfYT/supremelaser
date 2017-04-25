5eb199ef8e088e8714006c0241e0040a048fe4c3
Ty Roth <teelar1337@gmail.com>
2017-04-24 19:23:29 -0700

    Removed dev capes

--- src/main/java/com/ninjawarrior1337/supremelaser/proxy/ClientProxy.java
+++ src/main/java/com/ninjawarrior1337/supremelaser/proxy/ClientProxy.java
@@ -1,6 +1,5 @@
 package com.ninjawarrior1337.supremelaser.proxy;
 
-import com.jadarstudios.developercapes.DevCapes;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -20,7 +19,6 @@ public class ClientProxy extends CommonProxy
     public void init(FMLInitializationEvent e)
     {
         super.init(e);
-        DevCapes.getInstance().registerConfig("https://gist.githubusercontent.com/ninjawarrior1337/2ff29af4dc92683107fd9caeabf27f30/raw/e3296401cda83a88c30572865e68c911244b4036/capes.json", "supreme_capes");
     }
 
     @Override
